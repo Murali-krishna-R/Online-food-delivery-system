1c5d30d8452a7c0bcb461972320b5d4e1fe53584
R.Murali Krishna <310621104099@eec.srmrmp.edu.in>
2025-04-24 14:06:27 +0530

    First commit
    -Passed all 26 testcases

---
+++ Online Food Delivery System/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.examly.service;
+
+import java.util.List;
+
+import com.examly.entity.Order;
+import com.examly.entity.OrderItem;
+
+public interface OrderService {
+    boolean createOrder(Order order, List<OrderItem> orderedItems);
+    List<Order> getOrdersByCustomer(int customerId);
+}
